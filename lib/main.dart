9d60e4019cc100abf79018f90f16a710f2368215
Shashwat Rai <shashwatrai575@gmail.com>
2023-09-04 22:11:14 +0530

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
        debugShowCheckedModeBanner: false,
-      title: 'Flutter Chat',
+      title: 'Chat',
       theme: ThemeData(
           primarySwatch: Colors.green,
           backgroundColor: Colors.greenAccent,
